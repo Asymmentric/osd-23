e0620afed63b4bbf504b5a27165ef1b4423ef506
Arun Maurya <arunmaurya621@gmail.com>
2023-05-18 07:53:24 +0530

    feat: modified initial two sections

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -115,10 +115,10 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2015,
-                month: 6,
-                day: 28,
-                hours: 0,
+                year: 2023,
+                month: 5,
+                day: 22,
+                hours: 10,
                 minutes: 0,
                 seconds: 0,
                 words: {
